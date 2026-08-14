789eb1c70e3bb2141c7254e38d36a0f9b11047c9
Hifza-AI <hifza.ai.dev@gmail.com>
2026-08-14 22:48:08 +0500

    Changes done.

--- STAGE_1/cleaner.py
+++ STAGE_1/cleaner.py
@@ -10,7 +10,7 @@ class TextCleaner:
     def __init__(self):
         pass
 
-    # ---------------------------------------------------------
+    # ----------------------------------------------------------
     def clean(self, extracted_reports):
         cleaned_reports = []
 
